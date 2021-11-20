2031511653f7edcde4b4bd0ba421e9b263da1d03
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-11-20 16:14:50 -0300

    Criei Projeto2
    Primeiros programas em c++

---
+++ Projeto2/olamundo.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+int main() {
+    // endl, faz com que pule uma linha
+    // std, especifica de onde a informação esta saíndo.
+    std::cout << "Olá Mundo! c++" << std::endl;
+    return 0;
+}

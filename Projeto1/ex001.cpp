859e715bed88e5c05bb7344ebbdb537aa88f9d4b
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-11-21 15:56:22 -0300

    Update

---
+++ Projeto1/ex001.cpp
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(void) {
+    
+
+    // String de 30 chars ou bits de memória. 
+    char nome[30] = "Jackson";
+
+    //definindo string na linguagem c
+    printf("O valor do nome e %s", nome);
+
+    return 0;
+}
---
+++ Projeto2/ex002.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main() {
+
+    string nome = "Jackson";
+
+    cout << "Meu nome é " << nome << endl;
+}
